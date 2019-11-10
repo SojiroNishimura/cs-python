3476ca0c53af030277c23d62ea46fed175e04530
snishimura <sjiro.nishimura@gmail.com>
2019-11-10 20:56:22 +0900

    Add MiniMax and UnitTests for Tic-Tac-Toe

---
+++ chapter8/minimax.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+from board import Piece, Board, Move
+
+
+# 元のプレイヤーの裁量結果となる手を探す
+def minimax(board: Board, maximizing: bool, original_player: Piece, max_depth: int = 8) -> float:
+    # 基底部：停止位置または最大深さ
+    if board.is_win or board.is_draw or max_depth == 0:
+        return board.evaluate(original_player)
+
+    # 再帰部：利得最大または相手の利得最小を狙う
+    if maximizing:
+        best_eval: float = float("-inf") # 任意の極小値
+        for move in board.legal_moves:
+            result: float = minimax(board.move(move), False, original_player, max_depth - 1)
+            best_eval = max(result, best_eval)
+        return best_eval
+    else: # 最小化
+        worst_eval: float = float("inf")
+        for move in board.legal_moves:
+            result = minimax(board.move(move), True, original_player, max_depth - 1)
+            worst_eval = min(result, worst_eval)
+        return worst_eval
+
+
+# 現在の位置でmax_depthまでの最良の手を探す
+def find_best_move(board: Board, max_depth: int = 8) -> Move:
+    best_eval: float = float("-inf")
+    best_move: Move = Move(-1)
+    for move in board.legal_moves:
+        result: float = minimax(board.move(move), False, board.turn, max_depth)
+        if result > best_eval:
+            best_eval = result
+            best_move = move
+    return best_move
